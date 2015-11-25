69f91e42b8b7c4a22de42c6ec8781f6beed18002
objektwerks <objektwerks@gmail.com>
2015-11-25 18:08:10 -0500

    added get or else to discount

--- src/test/scala/store/Store.scala
+++ src/test/scala/store/Store.scala
@@ -4,10 +4,10 @@ case class Product(name: String)
 
 case class Price(product: Product, amount: Double)
 
-case class Discount(price: Price, discount: Double)
+case class Discount(price: Price, discount: Option[Double])
 
 case class Basket(items: Vector[Discount]) {
-  def calculate: Vector[Double] = items.map(d => d.price.amount * d.discount)
+  def calculate: Vector[Double] = items.map(d => d.price.amount * d.discount.getOrElse(1.0))
 }
 
 case class Inventory(product: Product *)
