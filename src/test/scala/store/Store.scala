c890a3e48b44b984ee5abeca2c8934d0ae579807
objektwerks <objektwerks@gmail.com>
2015-11-25 18:10:42 -0500

    factored stock into inventory, regrettably so

--- src/test/scala/store/Store.scala
+++ src/test/scala/store/Store.scala
@@ -10,14 +10,12 @@ case class Basket(items: Vector[Discount]) {
   def calculate: Vector[Double] = items.map(d => d.price.amount * d.discount.getOrElse(1.0))
 }
 
-case class Inventory(product: Product *)
-
-case class Stock(quantity: Int, inventory: Inventory *)
+case class Inventory(product: Product, stock: Int)
 
 class Clerk {
   def checkout(basket: Basket): Vector[Double] = basket.calculate
 }
 
-case class Store(inventory: Inventory, stock: Stock, clerk: Clerk)
+case class Store(inventory: Inventory, clerk: Clerk)
 
 case class Experience(store: Store)
