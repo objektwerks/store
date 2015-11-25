0f0f1b87f7fa4fc71ab098ed7e62af0bc8e9f12d
objektwerks <objektwerks@gmail.com>
2015-11-25 18:02:01 -0500

    a store, first draft

---
+++ src/test/scala/store/Store.scala
@@ -0,0 +1,21 @@
+package store
+
+case class Product(name: String)
+
+case class Price(product: Product, amount: Double)
+
+case class Discount(price: Price, discount: Double)
+
+case class Basket(items: Vector[Discount]) {
+  def calculate: Vector[Double] = items.map(d => d.price.amount * d.discount)
+}
+
+case class Inventory(product: Product *)
+
+case class Stock(quantity: Int, inventory: Inventory *)
+
+class Clerk {
+  def checkout(basket: Basket): Vector[Double] = basket.calculate
+}
+
+case class Store(inventory: Inventory, stock: Stock, clerk: Clerk)
