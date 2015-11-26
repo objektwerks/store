2d8a8beba7c9b4147f03fe901141f862582d1dae
objektwerks <objektwerks@gmail.com>
2015-11-26 11:49:05 -0500

    reinstall os x; resulting in commit to src

--- src/test/scala/store/Store.scala
+++ src/test/scala/store/Store.scala
@@ -4,20 +4,10 @@ case class Product(name: String)
 
 case class Price(product: Product, amount: Double)
 
-case class Discount(price: Price, discount: Option[Double])
+case class Discount(price: Price, discount: Double)
 
-case class Basket(items: Vector[Discount]) {
-  def calculate: Vector[Double] = items.map(d => d.price.amount * d.discount.getOrElse(1.0))
-}
+case class Basket(items: Vector[Product])
 
-case class Inventory(product: Product, stock: Int)
-
-class Clerk {
-  def checkout(basket: Basket): Double = basket.calculate.sum
-}
-
-case class Store(inventory: Inventory, clerk: Clerk)
-
-case class Shopper(store: Store) {
-  def checkout(basket: Basket *): Double = store.clerk.checkout(Basket(Vector[Discount]()))
+case class Store(products: Vector[Product], prices: Vector[Price], discounts: Vector[Discount]) {
+  def checkout(basket: Basket *): Double = 0.0
 }
