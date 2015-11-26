dcc8e8b3a858d5a793113ea88d999963e58db13c
objektwerks <objektwerks@gmail.com>
2015-11-26 12:08:18 -0500

    rename val

--- src/test/scala/store/Store.scala
+++ src/test/scala/store/Store.scala
@@ -4,7 +4,7 @@ case class Product(name: String, price: Double)
 
 case class Discount(product: Product, discount: Double)
 
-case class Basket(items: Vector[Product])
+case class Basket(products: Vector[Product])
 
 case class Store(products: Vector[Product], discounts: Vector[Discount]) {
   def checkout(basket: Basket *): Double = 0.0
