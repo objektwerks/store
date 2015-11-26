50e9491650779c5e144d3077ae5a9ac47b38605b
objektwerks <objektwerks@gmail.com>
2015-11-26 12:00:50 -0500

    sync

--- src/test/scala/store/Store.scala
+++ src/test/scala/store/Store.scala
@@ -1,12 +1,14 @@
 package store
 
+import java.time.LocalDateTime
+
 case class Product(name: String)
 
-case class Price(product: Product, amount: Double)
+case class Price(product: Product, amount: Double, date: LocalDateTime = LocalDateTime.now())
 
-case class Discount(price: Price, discount: Double)
+case class Discount(price: Price, discount: Double, date: LocalDateTime = LocalDateTime.now())
 
-case class Basket(items: Vector[Product])
+case class Basket(items: Vector[Product], date: LocalDateTime = LocalDateTime.now())
 
 case class Store(products: Vector[Product], prices: Vector[Price], discounts: Vector[Discount]) {
   def checkout(basket: Basket *): Double = 0.0
