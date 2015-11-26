f401462620f6e91b4e7af920a0e5189b166b2dbe
objektwerks <objektwerks@gmail.com>
2015-11-26 12:03:26 -0500

    simplified model

--- src/test/scala/store/Store.scala
+++ src/test/scala/store/Store.scala
@@ -1,15 +1,11 @@
 package store
 
-import java.time.LocalDateTime
+case class Product(name: String, price: Double)
 
-case class Product(name: String)
+case class Discount(product: Product, discount: Double)
 
-case class Price(product: Product, amount: Double, date: LocalDateTime = LocalDateTime.now())
+case class Basket(items: Vector[Product])
 
-case class Discount(price: Price, discount: Double, date: LocalDateTime = LocalDateTime.now())
-
-case class Basket(items: Vector[Product], date: LocalDateTime = LocalDateTime.now())
-
-case class Store(products: Vector[Product], prices: Vector[Price], discounts: Vector[Discount]) {
+case class Store(products: Vector[Product], discounts: Vector[Discount]) {
   def checkout(basket: Basket *): Double = 0.0
 }
