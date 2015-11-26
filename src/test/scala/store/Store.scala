4a1457c8d72110d7f73175270105dd27f30bbab8
objektwerks <objektwerks@gmail.com>
2015-11-26 16:05:05 -0500

    simplified model even further

--- src/test/scala/store/Store.scala
+++ src/test/scala/store/Store.scala
@@ -1,11 +1,9 @@
 package store
 
-case class Product(name: String, price: Double)
-
-case class Discount(product: Product, discount: Double)
+case class Product(name: String, price: Double, discount: Double = 0.0)
 
 case class Basket(products: Vector[Product])
 
-case class Store(products: Vector[Product], discounts: Vector[Discount]) {
+case class Store(products: Vector[Product]) {
   def checkout(basket: Basket *): Double = 0.0
 }
