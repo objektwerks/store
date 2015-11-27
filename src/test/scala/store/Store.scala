bb50b711a42bd6a113faeb34cbe762a4f0b8e139
objektwerks <objektwerks@gmail.com>
2015-11-27 08:28:21 -0500

    refactored store

--- src/test/scala/store/Store.scala
+++ src/test/scala/store/Store.scala
@@ -1,9 +1,27 @@
 package store
 
-case class Product(name: String, price: Double, discount: Double = 0.0)
+trait Product
+trait Apple extends Product
+trait Banana extends Product
+trait Grapes extends Product
+trait Kiwi extends Product
+trait Orange extends Product
+trait Pear extends Product
+trait Strawberries extends Product
+trait Champagne extends Product
 
-case class Basket(products: Vector[Product])
+case class Price(price: Double)
 
-case class Store(products: Vector[Product]) {
-  def checkout(basket: Basket *): Double = 0.0
+case class Entry(product: Product, price: Price)
+
+case class Discount(product: Product, quantity: Int, discount: Double)
+
+case class Bundle(products: Set[Product], price: Double)
+
+case class Catalog(entries: Set[Entry], discounts: Set[Discount], bundles: Set[Bundle])
+
+case class Cart(entries: Vector[Entry])
+
+class Store(catalog: Catalog) {
+  def checkout(cart: Cart): Double = 0.0
 }
