69e3ca42164afd039c7043092600cd7a08c5e8e7
objektwerks <objektwerks@gmail.com>
2015-11-28 14:25:01 -0500

    added discount and bundle tests

--- src/test/scala/store/StoreTest.scala
+++ src/test/scala/store/StoreTest.scala
@@ -3,6 +3,21 @@ package store
 import org.scalatest.FunSuite
 
 class StoreTest extends FunSuite{
-  test("store") {
+  test("discount") {
+    val discount = Discount(Key.Brie, 2, 0.10)
+    val discountedAmount = discount.apply(Key.Brie, 10.0, 2)
+    val discountedTotal = (10.0 * 2) - discountedAmount
+    assert(discountedAmount == 2.0)
+    assert(discountedTotal == 18.0)
+  }
+
+  test("bundle") {
+    val bundle = Bundle(Set(Key.Champagne, Key.Brie, Key.Truffles, Key.Strawberries), 0.10)
+    val discountedPercentage = bundle.apply(Set(Key.Champagne, Key.Brie, Key.Truffles, Key.Strawberries))
+    assert(discountedPercentage == 0.1)
+  }
+
+  test("cart") {
+    
   }
 }
