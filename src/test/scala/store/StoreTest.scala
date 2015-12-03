a495d423f5e752163bbde47d5b7ee79845ba25df
objektwerks <objektwerks@gmail.com>
2015-12-03 08:20:43 -0500

    renamed tests

--- src/test/scala/store/StoreTest.scala
+++ src/test/scala/store/StoreTest.scala
@@ -10,7 +10,7 @@ import scala.util.{Failure, Success}
 class StoreTest extends FunSuite {
   private implicit val ec = ExecutionContext.global
 
-  test("discount") {
+  test("catalog discount") {
     val discount = Discount(Key.Brie, 2, 0.10)
     val discountedAmount = discount.apply(Key.Brie, 10.0, 2)
     val discountedTotal = (10.0 * 2) - discountedAmount
@@ -18,7 +18,7 @@ class StoreTest extends FunSuite {
     assert(discountedTotal == 18.0)
   }
 
-  test("bundle") {
+  test("catalog bundle") {
     val bundle = Bundle(Set(Key.Champagne, Key.Brie, Key.Truffles, Key.Strawberries), 0.10)
     val discountedPercentage = bundle.apply(Set(Key.Champagne, Key.Brie, Key.Truffles, Key.Strawberries))
     assert(discountedPercentage == 0.1)
