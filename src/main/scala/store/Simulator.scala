023a0e487b509e1d7a9917b9ef7a718cfe47aa6b
objektwerks <objektwerks@runbox.com>
2016-12-20 15:28:41 -0500

    refactored future code

--- src/main/scala/store/Simulator.scala
+++ src/main/scala/store/Simulator.scala
@@ -9,11 +9,10 @@ object Simulator extends App {
 
   val catalog = Catalog()
   val store = new Store(catalog)
-  val futures: List[Future[Receipt]] = createListOfFutureReceipt(store, shoppers = 1000)
-  val future: Future[List[Receipt]] = Future.sequence(futures)
-  future onComplete {
-    case Success(receipt) => println(s"*** Number of shoppers: ${futures.length}! ***")
+  val receipts = Future.sequence(createListOfFutureReceipt(store, shoppers = 1000))
+  receipts onComplete {
+    case Success(shoppers) => println(s"*** Number of shoppers: ${shoppers.length}! ***")
     case Failure(failure) => println(s"*** Simulation failed: ${failure.getMessage} ***")
   }
-  Await.ready(future, 3 seconds)
+  Await.ready(receipts, 3 seconds)
 }
