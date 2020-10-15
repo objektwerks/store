cdf9f5fdeba53eef010061e74abe5da9f89c34cc
objektwerks <objektwerks@runbox.com>
2020-10-15 15:17:43 -0400

    refactored Catalog

--- src/main/scala/store/Catalog.scala
+++ src/main/scala/store/Catalog.scala
@@ -1,6 +1,20 @@
 package store
 
-case class Catalog(products: Set[Product], discounts: Set[Discount], bundles: Set[Bundle])
+final case class Catalog(products: Set[Product], discounts: Set[Discount], bundles: Set[Bundle])
+
+final case class Discount(key: Key.Value, threshold: Int, discount: Double) {
+  def price(product: Key.Value, price: Double, quantity: Double): Double = {
+    if (key == product && quantity >= threshold)
+      quantity * (price * discount)
+    else 0.0
+  }
+}
+
+final case class Bundle(keys: Set[Key.Value], discount: Double) {
+  def price(products: Set[Key.Value]): Double = {
+    if (keys.intersect(products) == keys) discount else 0.0
+  }
+}
 
 object Catalog {
   def apply(): Catalog = {
@@ -25,19 +39,5 @@ object Catalog {
 
   private def bundles: Set[Bundle] = {
     Set[Bundle](Bundle(Set(Key.Champagne, Key.Brie, Key.Truffles, Key.Strawberries), 0.10))
-  }
-}
-
-case class Discount(key: Key.Value, threshold: Int, discount: Double) {
-  def price(product: Key.Value, price: Double, quantity: Double): Double = {
-    if (key == product && quantity >= threshold)
-      quantity * (price * discount)
-    else 0.0
-  }
-}
-
-case class Bundle(keys: Set[Key.Value], discount: Double) {
-  def price(products: Set[Key.Value]): Double = {
-    if (keys.intersect(products) == keys) discount else 0.0
   }
 }
